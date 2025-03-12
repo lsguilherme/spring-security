a6c975ea09db06dc4858c87cc9df0a475c50b5db
Guilherme Lacerda <83317386+lsguilherme@users.noreply.github.com>
2025-03-12 17:16:24 -0300

    add: created user

---
+++ src/main/java/com/spring/spring_security/entities/User.java
@@ -0,0 +1,31 @@
+package com.spring.spring_security.entities;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "USERS")
+public class User {
+
+    @Id
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
